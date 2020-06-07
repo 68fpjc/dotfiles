b9ae482fa5016bbdfe535e44f56cd9039c63a541
68fpjc <32516363+68fpjc@users.noreply.github.com>
2020-06-07 18:26:56 +0900

    fix symlink problem

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ cd
 # [ -d dotfiles ] || git clone https://github.com/68fpjc/dotfiles.git
 find dotfiles -maxdepth 1 -name '.*' -not -name '.' -not -name '.git' | while read A; do
   B=$(basename $A)
-  if [ -f $B -o -d $B ]; then
-    rm -fr $B
-    ln -s $A $B
-  fi
+  [ -f $B -o -d $B ] && rm -fr $B
+  ln -s $A $B
 done
-
