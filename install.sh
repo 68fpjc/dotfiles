33544028dddbe5071da783685198781b8783fb63
68fpjc <32516363+68fpjc@users.noreply.github.com>
2020-06-07 21:48:22 +0900

    uncomment

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 cd
-# [ -d dotfiles ] || git clone https://github.com/68fpjc/dotfiles.git
+[ -d dotfiles ] || git clone https://github.com/68fpjc/dotfiles.git
 find dotfiles -maxdepth 1 -name '.*' -not -name '.' -not -name '.git' | while read A; do
   B=$(basename $A)
   [ -f $B -o -d $B ] && rm -fr $B
